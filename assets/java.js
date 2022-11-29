461ec057daeaf03db1acd9749fae3df041bdeb8e
Juan Galvan <juang1908@icloud.com>
2022-11-29 13:06:48 -0600

    added APIkey

--- assets/java.js
+++ assets/java.js
@@ -1,1 +1,11 @@
-var APIKey = "2255048fd7298045502b182ef2cef8e9"; 
+var APIKey = "2255048fd7298045502b182ef2cef8e9";
+
+
+var city;
+
+
+
+var queryURL =  "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+
+fetch(queryURL)
